eb3cf35051c46654ec5cae0cb7e5e113d01fde33
Joeyeyey <jlee.605372@nevada.unr.edu>
2021-03-14 16:46:55 -0700

    conditionals for registered vs unregistered users

--- app/src/main/java/com/example/splittr/User.java
+++ app/src/main/java/com/example/splittr/User.java
@@ -3,17 +3,22 @@ package com.example.splittr;
 public class User {
     private int id;
     private String name;
+    private boolean registered;
 
     public User(String name) {
         this.name = name;
+        this.registered = false;
     }
 
     public User(int id, String name) {
         this.id = id;
         this.name = name;
+        this.registered = true;
     }
 
     public int getId() {
+        if (!this.registered)
+            return -1;
         return id;
     }
 
