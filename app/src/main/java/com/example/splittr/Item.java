3534d0d6726a5be47f1b1969de91a0bb5169fb78
Joeyeyey <jlee.605372@nevada.unr.edu>
2021-03-14 01:11:59 -0800

    created classes User and SplittrMath

--- app/src/main/java/com/example/splittr/Item.java
+++ app/src/main/java/com/example/splittr/Item.java
@@ -1,12 +1,14 @@
 package com.example.splittr;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class Item {
 
     private String name;
     private double cost;
     private boolean taxable;
+    private ArrayList<User> owners;
 
     public Item(String name, double cost, boolean taxable) {
         this.name = name;
@@ -36,5 +38,13 @@ public class Item {
 
     public void setTaxable(boolean taxable) {
         this.taxable = taxable;
+    }
+
+    public ArrayList<User> getOwners() {
+        return owners;
+    }
+
+    public void setOwners(ArrayList<User> owners) {
+        this.owners = owners;
     }
 }
